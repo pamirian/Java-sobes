f665e787014753540489e2691e128962e2151bb7
pamirian <mirzoshoev@gmail.com>
2023-12-22 01:16:11 +0100

    2 problems for today

---
+++ EasyProblems/SliceArray.java
@@ -0,0 +1,32 @@
+package EasyProblems;
+//Реализовать функцию slice, которая принимает массив array(int[]), числа from и to.
+// Функция должна вернуть элементы массива array от индекса from (включая from) и до to.
+
+public class SliceArray {
+
+    public static void main(String[] args) {
+        int array[] = {7, 5, 9, 1, 5, 9, 1, 4};
+        int[] new_array = slice(array, 0, 2);
+
+        for (int i = 0; i < new_array.length; i++) {
+            System.out.print(new_array[i] + " ");
+        }
+    }
+
+
+    public static int[] slice(int[] array, int from, int to) {
+        int number =0;
+        for (int i=from; i<to; i++){
+            number=number+1;
+        }
+
+        int[] arr2 = new int[number];
+
+            for (int j = 0, i=from; i<to && j < array.length-1; i++){
+                arr2[j] = array[i];
+                j++;
+        }
+        return arr2;
+    }
+
+}
